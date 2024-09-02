b24b3dbd80fd7c72ced1b2c0107543ba41c6b890
Yaser Andrade <yaser1222@hotmail.com>
2024-09-01 19:43:17 -0600

    CODE

---
+++ src/app/data.js
@@ -0,0 +1,143 @@
+const products = [
+    {
+        id:1,
+        tittle:'Intel Core i7 12700',
+        price:395,
+        urlimg:'https://m.media-amazon.com/images/I/61Xf15NZYIL._AC_UF894,1000_QL80_.jpg',
+        quantity:15
+    },
+    {
+        id:2,
+        tittle:'AMD RYZEN 7 7800X3D',
+        price:425,
+        urlimg:'https://www.razorman.net/reviewshardware/wp-content/uploads/AMD-RYZEN-7-7800X-3D-6.webp',
+        quantity:22
+    },
+    {
+        id:3,
+        tittle:'XPG Lancer RGB DRR5 6400MHz 32GB (2x16GB)',
+        price:150,
+        urlimg:'https://m.media-amazon.com/images/I/510fA0L432L._AC_UF1000,1000_QL80_.jpg',
+        quantity:75
+    },
+    {
+        id:4,
+        tittle:'INTEL XEON E3-1230V2',
+        price:850,
+        urlimg:'https://m.media-amazon.com/images/I/61CvRH3kP5L.jpg',
+        quantity:5
+    },
+    {
+        id:5,
+        tittle:'AMD THREADRIPPER PRO 3995WX',
+        price:999,
+        urlimg:'https://images.anandtech.com/doci/16478/Carou.jpg',
+        quantity:4
+    },
+    {
+        id:6,
+        tittle:'MSI GEFORCE RTX 4090 SUPRIM',
+        price:1500,
+        urlimg:'https://m.media-amazon.com/images/I/816hhOe0gVL._AC_UF1000,1000_QL80_.jpg',
+        quantity:7
+    },
+    {
+        id:7,
+        tittle:'ASUS TUF GAMING AMD RADEON RX 7900 XTX OC Edition 24GB',
+        price:999,
+        urlimg:'https://m.media-amazon.com/images/I/81il2WdPPJL.jpg',
+        quantity:10
+    },
+    {
+        id:8,
+        tittle:'INTEL PENTIUM GOLD G7400',
+        price:125,
+        urlimg:'https://http2.mlstatic.com/D_NQ_NP_818854-MLU70028245955_062023-O.webp',
+        quantity:45
+    },
+    {
+        id:9,
+        tittle:'AMD ATHLON 3000G',
+        price:100,
+        urlimg:'https://kpchardware.com/9573-large_default/amd-athlon-3000g-32ghz-2-nucleos-4-hilos-.jpg',
+        quantity:34
+    },
+    {
+        id:10,
+        tittle:'RYZEN 3 1200',
+        price:120,
+        urlimg:'https://m.media-amazon.com/images/I/41pgQMRJyCL._AC_UF350,350_QL80_.jpg',
+        quantity:37
+    },
+    {
+        id:11,
+        tittle:'INTEL CORE I3 2100',
+        price:80,
+        urlimg:'https://cdn.cpu-world.com/Images/uploaded/0000/48/L_00004884.jpg',
+        quantity:22
+    },
+    {
+        id:12,
+        tittle:'CRUCIAL 64GB KIT (32GBX2) DDR5-4800 SODIMM',
+        price:200,
+        urlimg:'https://content.crucial.com/content/dam/crucial/dram-products/laptop/images/product/crucial-ddr5-32gb-sodimm-kit2-image.psd.transform/medium-png/image.png',
+        quantity:9
+    },
+    {
+        id:13,
+        tittle:'SAMSUNG 990 PRO SSD 4TB PCIE4X4',
+        price:479,
+        urlimg:'https://m.media-amazon.com/images/I/611--VvDc4L._AC_SX466_.jpg',
+        quantity:12
+    },
+    {
+        id:14,
+        tittle:'ASUS ROG Ryujin III 360',
+        price:340,
+        urlimg:'https://m.media-amazon.com/images/I/51BffKINobL._AC_SX466_.jpg',
+        quantity:13
+    },
+    {
+        id:15,
+        tittle:'SEAGATE IronWolf 12TB NAS Internal HDD',
+        price:239,
+        urlimg:'https://m.media-amazon.com/images/I/71ZNqv54H4L._AC_SX466_.jpg',
+        quantity:58
+    },
+    {
+        id:16,
+        tittle:'Thermaltake Tower 500',
+        price:160,
+        urlimg:'https://m.media-amazon.com/images/I/71Yam2jlHzL._AC_SX679_.jpg',
+        quantity:16
+    },
+    {
+        id:17,
+        tittle:'INTEL Core i9-14900KS',
+        price:750,
+        urlimg:'https://m.media-amazon.com/images/I/51KxsgP0HlL._AC_SX679_.jpg',
+        quantity:11
+    },
+    {
+        id:18,
+        tittle:'AMD Ryzen™ 9 9950X',
+        price:623,
+        urlimg:'https://m.media-amazon.com/images/I/61dukbEGziL._AC_SX679_.jpg',
+        quantity:3
+    },
+    {
+        id:19,
+        tittle:'MSI X670E GAMING PLUS WIFI Gaming Motherboard',
+        price:280,
+        urlimg:'https://m.media-amazon.com/images/I/81PracXCxWL._AC_SX679_.jpg',
+        quantity:12
+    },
+    {
+        id:20,
+        tittle:'Corsair HX1500i ATX POWER SUPPLY',
+        price:400,
+        urlimg:'https://m.media-amazon.com/images/I/817diqXXPgL._AC_SX466_.jpg',
+        quantity:78
+    },
+];
+export default products;
